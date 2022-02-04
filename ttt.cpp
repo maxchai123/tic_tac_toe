82f6ef4fdb5dace853a3bd21162abaec98fc67ad
shaggy567 <maxlchai@utexas.edu>
2022-02-04 10:23:03 -0800

    Basic board functionality

---
+++ ttt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "ttt_functions.hpp"
+int main() {
+
+  Board new_board; 
+  new_board.display_board(); 
+  //new_board.display_number();
+
+}
