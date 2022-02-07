42fb56fde113aa3bbb62c0c4207dbc374739fe91
maxchai123 <62576022+maxchai123@users.noreply.github.com>
2022-02-07 14:22:12 -0800

    initial working version

--- ttt_functions.hpp
+++ ttt_functions.hpp
@@ -16,17 +16,41 @@ class Board {
         7    *    8    *    9
              *         *
     */
+private:
     // Attributes
     std::vector<std::vector<char>> vector_board;
-    int number;
+    
 public:
     
     // Constructor
     Board();
 
+    // Board Functions
     void display_board();
 
-    void display_number();
+    void update_board(int location, char type); 
+
+    char result_of_game();
+};
+
+
+
+class Player {
+
+private:
+    // Attributes
+    std::string name;
+
+public:
+    // Constructor 
+    Player(std::string new_name);
+
+    // Player Functions
+    std::string display_name();
+
+    void update_name(std::string new_name);
+    
+
 
 };
  
